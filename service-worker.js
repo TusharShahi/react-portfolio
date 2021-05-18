72bdb76cfe95a31d8263c76e6a603ba5f3bb5344
Tushar Shahi <tushar.shahi@learner.manipal.edu>
2021-05-19 00:58:31 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4eff7dfef8c1992f9266045cb4db934c.js"
+  "/precache-manifest.0aa7cc7103ffe85b8dedbe66af1e41e1.js"
 );
 
 self.addEventListener('message', (event) => {
